310abcf8d7084cab57e1f7a81bdb0e6d7d6eb4ae
S4j331 <sajeeltipu4903@gmail.com>
2023-12-30 00:37:17 +0500

    scrapes

---
+++ example.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://quotes.toscrape.com/")
+def get_author_name():
+    author_name = driver.find_element(by="class name", value="author")
+    return author_name.text
+name = get_author_name()
+print(name)
+def get_quotes():
+    quote = driver.find_element(by="class name", value="col-md-8")
+    return quote.text
+quote = get_quotes()  
+print(quote)
