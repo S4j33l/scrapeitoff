8143639aa74985375423b9f522b2c1f8b8317396
S4j331 <sajeeltipu4903@gmail.com>
2024-01-02 16:39:37 +0500

    making amends

--- example.py
+++ example.py
@@ -1,13 +1,17 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get("https://quotes.toscrape.com/")
-def get_author_name():
-    author_name = driver.find_element(by="class name", value="author")
-    return author_name.text
-name = get_author_name()
-print(name)
-def get_quotes():
-    quote = driver.find_element(by="class name", value="col-md-8")
-    return quote.text
-quote = get_quotes()
-print(quote)
+login_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/p/a")))
+print(login_button.text)
+login_button.click()
+main_heading = driver.find_element(By.LINK_TEXT, "Quotes to Scrape")
+print(main_heading.text)
