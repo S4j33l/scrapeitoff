28759508434d1ff925a2b1fd11941f93e2630db4
S4j331 <sajeeltipu4903@gmail.com>
2024-01-03 10:33:04 +0500

    sorry

--- chakra_scraping.py
+++ chakra_scraping.py
@@ -1,2 +1,4 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("https://chakra-ui.com/docs/styled-system/style-props")
--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://quotes.toscrape.com/")
 
 
 def systemicActions():
-    login_button = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/p/a")
+    login_button = driver.find_element(
+        By.XPATH, "/html/body/div/div[1]/div[2]/p/a")
     login_button.click()
     time.sleep(2)
     username_text_field = driver.find_element(By.XPATH, '//*[@id="username"]')
@@ -17,5 +17,6 @@ def systemicActions():
     username_text_field.send_keys("s4j337")
     password_text_field = driver.find_element(By.XPATH, '//*[@id="password"]')
     password_text_field.send_keys("admin")
+
 
 systemicActions()
