64b8846722b8bfbbc157d19eae7fba20f2072253
S4j331 <sajeeltipu4903@gmail.com>
2024-01-03 10:31:17 +0500

    systemic actions

--- example.py
+++ example.py
@@ -1,17 +1,21 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://quotes.toscrape.com/")
-login_button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/p/a")))
-print(login_button.text)
-login_button.click()
-main_heading = driver.find_element(By.LINK_TEXT, "Quotes to Scrape")
-print(main_heading.text)
+
+
+def systemicActions():
+    login_button = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/p/a")
+    login_button.click()
+    time.sleep(2)
+    username_text_field = driver.find_element(By.XPATH, '//*[@id="username"]')
+    print(username_text_field.is_displayed())
+    username_text_field.send_keys("s4j337")
+    password_text_field = driver.find_element(By.XPATH, '//*[@id="password"]')
+    password_text_field.send_keys("admin")
+
+systemicActions()
