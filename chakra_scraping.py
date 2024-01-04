4e6a5ab69a32e790fc906a4a7b031f1ec15bd3ff
S4j331 <sajeeltipu4903@gmail.com>
2024-01-04 15:27:22 +0500

    CHAKRAS YANDHI

--- chakra_scraping.py
+++ chakra_scraping.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 driver = webdriver.Chrome()
 driver.get("https://chakra-ui.com/docs/styled-system/style-props")
+my_table_element_list: WebElement = driver.find_elements(By.TAG_NAME, "tr")
+for web_element in my_table_element_list:
+    print(web_element.text)
