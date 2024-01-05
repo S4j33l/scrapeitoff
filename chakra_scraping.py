e2afaadeec9f7ec5990724396d91de9388867da4
S4j331 <sajeeltipu4903@gmail.com>
2024-01-05 15:35:20 +0500

    chakraing

--- chakra_scraping.py
+++ chakra_scraping.py
@@ -4,10 +4,10 @@ from selenium.webdriver.remote.webelement import WebElement
 
 driver = webdriver.Chrome()
 driver.get("https://chakra-ui.com/docs/styled-system/style-props")
-my_table_element_list: WebElement = driver.find_elements(By.TAG_NAME, "tr")
+my_charka_element_list: WebElement = driver.find_elements(By.TAG_NAME, "tr")
 
 with open("chakra_elements.txt", "w") as chakra_elements_file:
-    for web_element in my_table_element_list:
+    for web_element in my_charka_element_list:
         if not (web_element.text.startswith("Prop")):
             split_text = web_element.text.split(",", 1)
             if len(split_text) > 1:
